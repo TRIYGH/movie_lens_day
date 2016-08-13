95e8ffafc0ac8fc7fc66e345006a2fb6d719cfca
T. Robert Ward <trironyard@icloud.com>
2016-08-13 11:33:12 -0400

    built to import data, but can't get it to recognize my changes to 0002 - STUCK

--- movie_lens/movie_lens/urls.py
+++ movie_lens/movie_lens/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$movie_rate/', include('movie_rate.urls')),
+    url(r'^movie_rate/', include('movie_rate.urls')),
     url(r'^admin/', admin.site.urls),
 ]
